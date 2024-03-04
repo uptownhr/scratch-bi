23230411cf2f769e248b9d8a7b42fe66dde6737d
James <uptownhr@gmail.com>
2024-03-04 06:36:31 -0800

    modified opportunity sql

--- sources/postgres/opportunity.sql
+++ sources/postgres/opportunity.sql
@@ -1,1 +1,26 @@
-select * from public.opportunity
+select
+ id
+ , source_id
+ , organization_id
+ , ref_id
+ , ref_url
+ , name
+ , state
+ , city
+ , county
+ , address1
+ , address2
+ , postal_code
+ , contact_email
+ , contact_phone
+ , sector_type
+ , dollar_actual
+ , gpt_keyword_token
+ , gpt_description_token
+ , status
+ , posted_date
+ , begin_date
+ , end_date
+ , created
+
+from public.opportunity
